6ed3f440b115da4fd7ef09f9debb963bee2c5b6c
fatekkl <mathtml.1105@gmail.com>
2025-01-21 15:44:22 -0300

    update docs + work on journey

--- src/app/Components/Journey/index.tsx
+++ src/app/Components/Journey/index.tsx
@@ -1,38 +1,48 @@
 import DotSquare from "../DotSquare";
+import Menu from "../Menu";
 import Stair from "../Stair";
 
 export default function Journey() {
     return (
-        <section className="flex flex-col md:flex-row min-h-screen relative bg-jet">
-
-            {/* Elementos decorativos que podem ficar ocultos em telas muito pequenas */}
-            <div className="hidden md:block absolute left-2/4  top-[6rem]">
-                <DotSquare />
-            </div>
-            <div className="hidden md:block absolute left-[45rem] opacity-30 top-[32rem]">
-                <DotSquare />
-            </div>
-            <div className="hidden md:block absolute left-[25rem] top-[50rem]  opacity-30">
-                <DotSquare />
-            </div>
-            <div className="hidden md:block absolute left-[55rem] top-[48rem]">
-                <DotSquare />
+        <>
+            <div className="absolute z-10 ml-4 md:ml-12 mt-[50rem] -rotate-90">
+                <Stair />
             </div>
 
-            <div className="ml-12 mt-16 w-1/2 flex flex-col gap-4">
-                <div className="flex flex-col gap-2">
-                    <h1 className="text-tea text-5xl font-bold text-start">Minha jornada</h1>
-                    <p className="font-normal text-justify text-2xl w-[43.75rem] h-40">Ao longo dos anos já interagi e trabalhei com diversas tecnologias do mercado, como JavaScript, Kotlin, PHP, Java, entre outros. Como resultado, tive a oportunidade de trabalhar em diversos projetos pelo Brasil inteiro.</p>
+            <section className="flex justify-evenly md:flex-row min-h-screen relative bg-jet">
+                {/* Elementos decorativos que podem ficar ocultos em telas muito pequenas */}
+                <div className="hidden md:block absolute left-2/4  top-[6rem]">
+                    <DotSquare />
                 </div>
-                <div className="flex flex-col gap-2">
-                    <h2 className="text-tea text-[2rem] font-bold">O que fiz nesses projetos?</h2>
-                    <p className="font-normal text-justify text-2xl w-[43.75rem] h-40">Ao longo dos anos trabalhei nos mais diversos tipos de projeto, como integrações, configuração e hospedagem de servidores, automações de estoque, entre outros. Com isso, adquiri experiência para me adaptar a diversas situações e trabalhar de diferentes formas e tecnologias.</p>
+                <div className="hidden md:block absolute left-[50rem] opacity-30 top-[32rem]">
+                    <DotSquare />
                 </div>
-                <div className="flex flex-col gap-2">
-                    <h2 className="text-tea text-[2rem] font-bold">O que busco agora?</h2>
-                    <p className="font-normal text-justify text-2xl w-[43.75rem] h-40">Atualmente estou buscando a minha primeira oportunidade de emprego FIXO e prester a iniciar o meu ensino superior. Em paralelo a meus estudos sobre software escalável, estrutura de dados e UI/UX.</p>
+                <div className="hidden md:block absolute left-[25rem] top-[50rem]  opacity-30">
+                    <DotSquare />
                 </div>
-            </div>
-        </section>
+                <div className="hidden md:block absolute left-[55rem] top-[48rem]">
+                    <DotSquare />
+                </div>
+
+
+
+                <div className="ml-12 mt-16 w-1/2 flex flex-col gap-4">
+                    <div className="flex flex-col gap-2">
+                        <h1 className="text-tea text-5xl font-bold text-start">Minha jornada</h1>
+                        <p className="font-normal text-justify text-2xl w-[43.75rem] h-40">Ao longo dos anos já interagi e trabalhei com diversas tecnologias do mercado, como JavaScript, Kotlin, PHP, Java, entre outros. Como resultado, tive a oportunidade de trabalhar em diversos projetos pelo Brasil inteiro.</p>
+                    </div>
+                    <div className="flex flex-col gap-2">
+                        <h2 className="text-tea text-[2rem] font-bold">O que fiz nesses projetos?</h2>
+                        <p className="font-normal text-justify text-2xl w-[43.75rem] h-40">Ao longo dos anos trabalhei nos mais diversos tipos de projeto, como integrações, configuração e hospedagem de servidores, automações de estoque, entre outros. Com isso, adquiri experiência para me adaptar a diversas situações e trabalhar de diferentes formas e tecnologias.</p>
+                    </div>
+                    <div className="flex flex-col gap-2">
+                        <h2 className="text-tea text-[2rem] font-bold">O que busco agora?</h2>
+                        <p className="font-normal text-justify text-2xl w-[43.75rem] h-40">Atualmente estou buscando a minha primeira oportunidade de emprego FIXO e prester a iniciar o meu ensino superior. Em paralelo a meus estudos sobre software escalável, estrutura de dados e UI/UX.</p>
+                    </div>
+                </div>
+                <img src="../../../assets/Storyset.svg" className="mr-16">
+                </img>
+            </section>
+        </>
     );
 }
--- src/app/Components/Line/index.tsx
+++ src/app/Components/Line/index.tsx
@@ -2,7 +2,7 @@ export default function Line() {
     return (
         <div className="mr-5">
             <svg width="2" height="350" viewBox="0 0 2 364" fill="none" xmlns="http://www.w3.org/2000/svg">
-                <path d="M1 364L1 -9.86457e-06" stroke="#343330" strokeWidth="2" />
+                <path d="M1 364L1 -9.86457e-06" stroke="#343330" strokeWidth="4" />
             </svg>
         </div>
     )
