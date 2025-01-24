ece0547882f223ac37844a2620fa90fdefd49f5c
fatekkl <mathtml.1105@gmail.com>
2025-01-24 16:14:26 -0300

    create new svg component

--- src/app/Components/Line2/Line2.tsx
+++
@@ -1,8 +0,0 @@
-export default function Line2() {
-    return (
-        <svg width="314" height="5" viewBox="0 0 314 5" fill="none" xmlns="http://www.w3.org/2000/svg">
-            <line x1="0.192383" y1="2.42261" x2="314" y2="2.42261" stroke="#C7FFDA" stroke-width="4" />
-        </svg>
-
-    )
-}
