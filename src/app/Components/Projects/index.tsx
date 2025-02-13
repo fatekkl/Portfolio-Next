52567f9dfb54c5d4b4a75f54a47107afd9fa1b57
fatekkl <mathtml.1105@gmail.com>
2025-02-13 16:05:33 -0300

    fix minor error

--- src/app/Components/Projects/index.tsx
+++ src/app/Components/Projects/index.tsx
@@ -200,7 +200,7 @@ export default function Projects() {
             </section>
           </div>
 
-          <div className={`${isMobile ? "flex flex-col items-center justify-around w-full h-full" : "max-md:hidden"} `}>
+          <div className={`${isMobile ? "flex flex-col items-center justify-around w-full h-full" : "hidden"} `}>
                 <MobileProject projectTitle="Resend"/>
                 <MobileProject projectTitle="Ozon Solution"/>
                 <MobileProject projectTitle="Bling"/>
