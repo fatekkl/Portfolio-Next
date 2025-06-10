d115c647d50c1d0b6ba5d08bc8efe82b6303ed43
fatekkl <mathtml.1105@gmail.com>
2025-06-10 15:22:18 -0300

    remove unused + refactor main page

--- src/app/Components/Journey/index.tsx
+++ src/app/Components/Journey/index.tsx
@@ -1,40 +1,32 @@
-import Stair from "../Stair";
 
 export default function Journey() {
     return (
-        <>
-            <aside className="fixed bottom-0 left-0 mb-4 ml-4 md:ml-12 z-40 -rotate-90 max-lg:hidden">
-                <Stair />
-            </aside>
+        <main className="flex flex-col md:flex-row justify-around min-h-screen relative bg-jet p-8">
 
+            <article className="md:ml-12 mt-16 max-sm:w-full md:w-1/2 flex flex-col gap-8 tracking-[0.094rem]">
+                <section className="flex flex-col gap-2 md:text-center lg:text-justify">
+                    <h2 className="text-green_secondary max-sm:text-center text-3xl font-bold">Com o que trabalho?</h2>
+                    <p className="font-normal text-silver max-sm:text-center  max-sm:text-xl md:text-2xl h-auto">
+                        Ao longo dos anos já interagi e trabalhei com diversas tecnologias do mercado, como JavaScript, Kotlin, PHP, Java, Python, .Net, entre outros. Como resultado, tive a oportunidade de trabalhar em diversos projetos pelo Brasil inteiro.
+                    </p>
+                </section>
+                <section className="flex flex-col gap-2 md:text-center lg:text-justify text-green_secondary">
+                    <h2 className="text-green_secondary max-sm:text-center text-3xl font-bold">O que fiz nesses projetos?</h2>
+                    <p className="font-normal text-silver max-sm:text-center  max-sm:text-xl md:text-2xl max-w-full h-auto">
+                        Já trabalhei nos mais diversos tipos de projetos, como integrações, configuração e hospedagem de servidores, automações de estoque, entre outros. Com isso, adquiri experiência para me adaptar a diversas situações e trabalhar de diferentes formas e tecnologias.
+                    </p>
+                </section>
+                <section className="flex flex-col gap-2 md:text-center lg:text-justify text-green_secondary">
+                    <h2 className="text-green_secondary max-sm:text-center  text-3xl font-bold">O que busco agora?</h2>
+                    <p className="font-normal text-silver max-sm:text-center  max-sm:text-xl md:text-2xl max-w-70 h-auto">
+                        Atualmente, estou em busca da minha primeira oportunidade de emprego em tempo integral e me preparo para ingressar no ensino superior. Paralelamente, sigo aprimorando meus conhecimentos em software escalável, estrutura de dados e UI/UX.
+                    </p>
+                </section>
+            </article>
 
-            <main className="flex flex-col md:flex-row justify-around min-h-screen relative bg-jet p-8">
-                
-                <article className="md:ml-12 mt-16 max-sm:w-full md:w-1/2 flex flex-col gap-8 tracking-[0.094rem]">
-                    <section className="flex flex-col gap-2 md:text-center lg:text-justify">
-                        <h2 className="text-green_secondary max-sm:text-center text-3xl font-bold">Com o que trabalho?</h2>
-                        <p className="font-normal text-silver max-sm:text-center  max-sm:text-xl md:text-2xl h-auto">
-                            Ao longo dos anos já interagi e trabalhei com diversas tecnologias do mercado, como JavaScript, Kotlin, PHP, Java, Python, .Net, entre outros. Como resultado, tive a oportunidade de trabalhar em diversos projetos pelo Brasil inteiro.
-                        </p>
-                    </section>
-                    <section className="flex flex-col gap-2 md:text-center lg:text-justify text-green_secondary">
-                        <h2 className="text-green_secondary max-sm:text-center text-3xl font-bold">O que fiz nesses projetos?</h2>
-                        <p className="font-normal text-silver max-sm:text-center  max-sm:text-xl md:text-2xl max-w-full h-auto">
-                            Já trabalhei nos mais diversos tipos de projetos, como integrações, configuração e hospedagem de servidores, automações de estoque, entre outros. Com isso, adquiri experiência para me adaptar a diversas situações e trabalhar de diferentes formas e tecnologias.
-                        </p>
-                    </section>
-                    <section className="flex flex-col gap-2 md:text-center lg:text-justify text-green_secondary">
-                        <h2 className="text-green_secondary max-sm:text-center  text-3xl font-bold">O que busco agora?</h2>
-                        <p className="font-normal text-silver max-sm:text-center  max-sm:text-xl md:text-2xl max-w-70 h-auto">
-                            Atualmente, estou em busca da minha primeira oportunidade de emprego em tempo integral e me preparo para ingressar no ensino superior. Paralelamente, sigo aprimorando meus conhecimentos em software escalável, estrutura de dados e UI/UX.
-                        </p>
-                    </section>
-                </article>
-
-                <figure className="self-center w-96 h-auto max-lg:hidden z-40">
-                    <img src="../../../assets/Storyset.svg" alt="Ilustração de jornada profissional" />
-                </figure>
-            </main>
-        </>
+            <figure className="self-center w-96 h-auto max-lg:hidden z-40">
+                <img src="../../../assets/Storyset.svg" alt="Ilustração de jornada profissional" />
+            </figure>
+        </main>
     );
 }
--- src/app/Components/Principal/index.tsx
+++ src/app/Components/Principal/index.tsx
@@ -1,139 +1,55 @@
 "use client";
 
-import Line from "../Line";
-import Stair from "../Stair";
-import Link from "next/link";
-import Image from "next/image";
-import React, { useEffect, useState } from "react";
+import React from "react";
 
 export default function Principal() {
-  const [color, setColor] = useState("#343330")
 
 
-  const handleCopy = (
-    event: React.MouseEvent<HTMLDivElement>,
-    colorClass: string
-  ): void => {
-    const { currentTarget } = event;
-    navigator.clipboard.writeText(currentTarget.innerText);
-    currentTarget.classList.replace("text-violet", colorClass);
-    currentTarget.classList.add("scale-105")
-    setTimeout(() => currentTarget.classList.replace(colorClass, "text-violet"), 700);
-    setTimeout(() => currentTarget.classList.remove("scale-105"), 700);
-  };
 
-  useEffect(() => {
-    const handleScroll = () => {
-      const scrollPosition = window.scrollY;
-
-      if (scrollPosition < 840) {
-        setColor("#343330");
-      } else {
-        setColor("#EBEDC5");
-      }
-    };
-
-    window.addEventListener("scroll", handleScroll);
-
-    // Remove o listener ao desmontar o componente
-    return () => {
-      window.removeEventListener("scroll", handleScroll);
-    };
-  }, []);
 
   return (
     <main id="principal" className="flex relative">
       {/* Seção de apresentação */}
-      <section className="w-screen md:w-2/3 lg:w-2/3 h-auto bg-jet relative flex flex-col max-sm:gap-2 gap-8 max-sm:items-center">
-        <header className="absolute ml-4 lg:ml-12 mt-8 max-sm:hidden">
-          <Stair />
-        </header>
-
-        <article className="w-full h-3/4 flex flex-col justify-end gap-6 sm:justify-center sm:items-center md:items-start md:justify-end max-sm:items-center text-center max-sm:justify-center">
-          <h1 className="font-bold text-silver ml-12 text-xl lg:text-4xl leading-normal max-sm:text-center md:text-start">
-            Olá,
-            <br />
-            Sou o Matheus 👋
-          </h1>
-          <h2 className="font-bold ml-12 text-2xl text-green_secondary max-sm:text-center">
-            Desenvolvedor Fullstack
-          </h2>
-          <p className="lg:w-2/4 ml-12 md:w-2/4 text-xl text-silver text-justify sm:text-center md:text-start sm:w-6/12 max-sm:w-8/12 max-sm:text-center">
-            Sou um desenvolvedor curioso e apaixonado por tecnologia, conheça mais sobre minhas
-            habilidades e projetos aqui.
+      <section className="w-1/2 h-screen bg-jet flex flex-col">
+        <div className="flex flex-col justify-center p-8 mt-10 relative">
+          <h1 className="text-4xl md:text-5xl font-bold mb-2">Olá,</h1>
+          <h2 className="text-4xl text-silver md:text-5xl font-bold mb-4">Sou o Matheus 👋</h2>
+          <p className="text-green_secondary text-lg md:text-xl font-semibold mb-6">Desenvolvedor Fullstack</p>
+          <p className="text-silver mb-8 text-base md:text-lg leading-relaxed">
+            Sou um desenvolvedor curioso e apaixonado por tecnologia, conheça mais sobre minhas habilidades e projetos aqui.
           </p>
-          <nav className="ml-12 flex gap-5" aria-label="links sociais">
-            <Link
-              href="https://www.linkedin.com/in/matheusolvcosta/"
-              target="_blank"
-              rel="noopener noreferrer"
-              className="hover:scale-125 transition-transform duration-300"
-            >
-              <Image
-                src="/assets/Linkedin.svg"
-                alt="LinkedIn"
-                width={64}
-                height={64}
-                className="w-10 lg:w-16"
-              />
-            </Link>
-            <Link
-              href="https://github.com/fatekkl"
-              target="_blank"
-              rel="noopener noreferrer"
-              className="hover:scale-125 transition-transform duration-300"
-            >
-              <Image
-                src="/assets/Github.svg"
-                alt="GitHub"
-                width={64}
-                height={64}
-                className="w-10 lg:w-16"
-              />
-            </Link>
-            <a
-              href="/MatheusCurriculo.pdf"
-              download
-              className="hover:scale-125 transition-transform duration-300"
-            >
-              <Image
-                src="/assets/Document.svg"
-                alt="Currículo"
-                width={64}
-                height={64}
-                className="w-10 lg:w-16"
-              />
+          <div className="flex space-x-4 mb-12">
+            <a href="https://www.linkedin.com/in/matheusolvcosta/" target="_blank" rel="noopener noreferrer">
+              <img className="hover:scale-110 transition-all duration-150" src="../assets/Linkedin.svg" width={42} height={42} />
             </a>
-          </nav>
-        </article>
-        <div className="text-gold mt-4 font-bold max-sm:w-1/2 w-full h-fit flex flex-col items-center justify-center gap-4 max-sm:text-xs text-xl tracking-[0.094rem]">
-          <span className="text-wrap text-center">
-            Solucione seus problemas com software.
-          </span>
-          <div className="flex justify-between text-xs font-normal text-wrap h-full max-sm:gap-4 gap-8 text-center mb-4">
-            <span>Desenvolto com escalabilidade e performance.</span>
-            <span>Trabalhei em projetos por todo o Brasil.</span>
+            <a href="https://github.com/fatekkl" target="_blank" rel="noopener noreferrer">
+              <img className="hover:scale-110 transition-all duration-150" src="../assets/Github.svg" width={42} height={42} />
+            </a>
+            <a href="../MatheusCurriculo.pdf" download={true}>
+              <img className="hover:scale-110 transition-all duration-150" src="../assets/Document.svg" width={42} height={42} />
+            </a>
+
+          </div>
+
+
+        </div>
+
+        <div className="mt-auto p-12 ">
+          <h3 className="text-xl font-semibold mb-3">Solucione seus problemas com software.</h3>
+          <div className="grid grid-cols-1 sm:grid-cols-2 gap-4 text-sm text-silver">
+            <p>Desenvolvo com escalabilidade e performance.</p>
+            <p>Trabalhei em projetos por todo o Brasil.</p>
           </div>
         </div>
+
       </section>
 
-      <figure className="absolute top-1/2 sm:right-1/4 left-2/3 transform -translate-y-1/2 -translate-x-1/2 z-40 max-md:hidden md:w-[14rem] lg:w-[20rem]">
-        <Image
-          src="/assets/Eu.png"
-          alt="Foto de Matheus"
-          width={320}
-          height={320}
-          className="w-full h-auto"
-        />
-      </figure>
-
       {/* Seção lateral */}
-      <aside className="bg-green_secondary sm:w-1/6 md:w-1/3 lg:w-1/3 flex flex-col h-screen justify-end">
-        <footer className="w-full justify-end hidden lg:flex">
-          <Line />
-        </footer>
-       
+      <aside className="w-1/2 h-screen bg-green_secondary flex justify-center items-center">
+        <img src="../assets/MainPhoto.png" />
+
+
       </aside>
-    </main>
+    </main >
   );
 }
