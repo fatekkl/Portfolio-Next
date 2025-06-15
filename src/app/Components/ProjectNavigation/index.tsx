8f07eab06794f1a62ae28c66587f176809ac4a7a
fatekkl <mathtml.1105@gmail.com>
2025-06-15 12:20:44 -0300

    turn projects responsive

--- src/app/Components/ProjectNavigation/index.tsx
+++ src/app/Components/ProjectNavigation/index.tsx
@@ -1,31 +1,36 @@
 import { Dispatch, SetStateAction } from "react";
 import projects from '../../../../public/projects.json';
 
-
 interface Props {
-    activeProject: number;
-    setActiveProject: Dispatch<SetStateAction<number>>;
+  activeProject: number;
+  setActiveProject: Dispatch<SetStateAction<number>>;
 }
 
 export default function ProjectNavigation({ activeProject, setActiveProject }: Props) {
-    function handleActiveProject(e: React.MouseEvent<HTMLAnchorElement>) {
-        e.preventDefault();
-        const id = Number(e.currentTarget.id);
-        setActiveProject(id);
-    }
+  function handleActiveProject(e: React.MouseEvent<HTMLAnchorElement>) {
+    e.preventDefault();
+    const id = Number(e.currentTarget.id);
+    setActiveProject(id);
+  }
 
-    function getClassName(id: number) {
-        return `block px-4 py-3 cursor-pointer rounded-lg font-medium hover-link ${activeProject === id ? "active-link text-white" : "text-gray-400"
-            }`;
-    }
+  function getClassName(id: number) {
+    return `block px-3 py-2 sm:px-4 sm:py-3 cursor-pointer rounded-lg font-medium transition-all duration-200 hover-link ${
+      activeProject === id ? "active-link text-white" : "text-gray-400"
+    }`;
+  }
 
-    return (
-        <nav className="md:w-1/4 space-y-2">
-
-            {projects.map((project, index) => {
-                return <a id={index.toString()} key={index.toString()} onClick={handleActiveProject} className={getClassName(index)}>{project.company}</a>
-
-            })}
-        </nav>
-    );
+  return (
+    <nav className="w-full md:w-1/4 space-y-2">
+      {projects.map((project, index) => (
+        <a
+          id={index.toString()}
+          key={index}
+          onClick={handleActiveProject}
+          className={getClassName(index)}
+        >
+          {project.company}
+        </a>
+      ))}
+    </nav>
+  );
 }
--- src/app/Components/ProjectTask/index.tsx
+++ src/app/Components/ProjectTask/index.tsx
@@ -1,13 +1,18 @@
 interface ProjectTaskProps {
-    description: string;
+  description: string;
 }
 
 export default function ProjectTask({ description }: ProjectTaskProps) {
-    return (
-        <li className="flex items-center gap-2">
-            <img src="../assets/task_alt.svg" />
-            <span className="leading-relaxed">{description}</span>
-        </li>
-        
-    );
+  return (
+    <li className="flex items-start gap-2">
+      <img
+        src="../assets/task_alt.svg"
+        alt="task"
+        className="w-5 h-5 sm:w-6 sm:h-6"
+      />
+      <span className="leading-relaxed text-sm sm:text-base">
+        {description}
+      </span>
+    </li>
+  );
 }
